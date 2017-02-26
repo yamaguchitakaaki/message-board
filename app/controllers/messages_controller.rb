1083b5a9b3147dfdcc87bfd6ffcdb4c426a351a2
takaaki yamaguchi <kenmei.takaaki@gmail.com>
2017-02-26 14:58:07 +0000

    variation

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,11 +6,17 @@ class MessagesController < ApplicationController
     @messages = Message.all
   end
 
-  def create
+def create
     @message = Message.new(message_params)
-    @message.save
-    redirect_to root_path , notice: 'メッセージを保存しました'
-  end
+    if @message.save
+      redirect_to root_path , notice: 'メッセージを保存しました'
+    else
+      # メッセージが保存できなかった時
+      @messages = Message.all
+      flash.now[:alert] = "メッセージの保存に失敗しました。"
+      render 'index'
+    end
+end
 
   private
   def message_params

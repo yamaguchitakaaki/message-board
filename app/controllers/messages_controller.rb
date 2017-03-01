07748e04e1286a5b6e4b6f9f7a1bd48883a0a03f
takaaki yamaguchi <kenmei.takaaki@gmail.com>
2017-03-01 13:11:37 +0000

    edit_delete

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,24 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:edit, :update, :destroy]
+
   def index
 
     @message = Message.new
     # Messageを全て取得する。
     @messages = Message.all
+  end
+
+def edit
+  end
+  
+  def update
+    if @message.update(message_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to root_path , notice: 'メッセージを編集しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
   end
 
 def create
@@ -18,9 +33,22 @@ def create
     end
 end
 
+def destroy
+  @message.destroy
+  redirect_to root_path, notice:'メッセージを削除しました'
+
+end
+
   private
   def message_params
     params.require(:message).permit(:name, :body)
   end
   ## ここまで
+
+  def set_message
+    @message = Message.find(params[:id])
+  end
+
+
 end
+
